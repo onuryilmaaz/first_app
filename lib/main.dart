87329fa3a479ca1ccd154a12ff04196eee981ee5
onuryilmaaz <onuryilm.41@gmail.com>
2025-03-26 14:43:59 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: 
+    Scaffold(
+      body: 
+      GradientContainer())));
+}
